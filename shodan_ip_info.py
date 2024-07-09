4f9e701d7f9b80e24ba2765f0418e420ccceb416
Bert de Jong <68970119+Cyb3rtus@users.noreply.github.com>
2024-07-09 13:21:06 +0200

    Update shodan_ip_info.py
    Updated while loop and added the sleep timer cause Shodan got mad at me.

--- shodan_ip_info.py
+++ shodan_ip_info.py
@@ -3,10 +3,12 @@ import shodan
 import sys
 api = shodan.Shodan('YOUR_API_KEY_HERE')
 
-# this script print out properties of an IP address through the Shodan API.
+# This script prints out properties of an IP address through the Shodan API.
 # Do you have several IPs in a file? Do:
-# "while read -r ips.txt; do ./shodan_ip_info.py $ips; done < ips.txt"
-# Make sure to make the script executable with "chmod +x"
+# while read -r ips; do ./shodan_ip_info.py $ips; sleep 2; done < your_ip_file.txt > shodan_scan.txt
+# this reads from a file of IPs, puts a 2 second sleep per request (shodan asks for a 1 second rate limit) and 
+# writes the output to a text file
+# Make sure to make the script executable with "chmod +x shodan_ip_info.py"
 
 
 
